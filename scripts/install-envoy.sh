85a44a8a533b2f14cb53ff30cde3e13f6d855578
Talha Waheed <talhawaheed57@gmail.com>
2023-03-14 14:14:26 -0500

    Tweaked to work on the kubernetes setup

--- scripts/install-envoy.sh
+++ scripts/install-envoy.sh
@@ -5,5 +5,5 @@ curl -sL 'https://deb.dl.getenvoy.io/public/gpg.8115BA8E629CC074.key' |  gpg --d
 Verify the keyring - this should yield "OK"
 echo a077cb587a1b622e03aa4bf2f3689de14658a9497a9af2c427bba5f4cc3c4723 /usr/share/keyrings/getenvoy-keyring.gpg | sha256sum --check
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/getenvoy-keyring.gpg] https://deb.dl.getenvoy.io/public/deb/ubuntu $(lsb_release -cs) main" |  tee /etc/apt/sources.list.d/getenvoy.list
-apt update
-apt install -y getenvoy-envoy
+apt-get update
+apt-get install -y getenvoy-envoy
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# add env variables to cds.yaml
+cp /work/envoy-config/cds.yaml /work/envoy-config/_cds.yaml
+cat /work/envoy-config/_cds.yaml | envsubst \$ENVOY_LB_ALG,\$TIMEOUT > /work/envoy-config/cds.yaml
+rm /work/envoy-config/_cds.yaml
+
+# add env variables to lds.yaml
+cp /work/envoy-config/lds.yaml /work/envoy-config/_lds.yaml
+cat /work/envoy-config/_lds.yaml | envsubst \$ENVOY_LB_ALG,\$TIMEOUT > /work/envoy-config/lds.yaml
+rm /work/envoy-config/_lds.yaml
+
 # Start Envoy
 envoy -c /work/envoy-config/config.yaml -l debug 2> /tmp/envoy.log &
 
